f7b997bda5bad2cd8b6839bfc4a6f8d82d46ddba
Nwitlyck <davidsegura118@hotmail.com>
2023-11-19 12:38:07 -0600

    Initial commit

---
+++ src/main/java/edu/ulatina/APIEncryption/ApiEncryptionApplication.java
@@ -0,0 +1,13 @@
+package edu.ulatina.APIEncryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ulatina/APIEncryption/ApiEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ulatina.APIEncryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
