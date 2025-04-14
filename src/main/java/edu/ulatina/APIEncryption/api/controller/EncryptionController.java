9faa381f7b9111705ca3ff4b5d0f5bc6bb447e83
Nwitlyck <davidsegura118@hotmail.com>
2025-04-14 16:10:21 -0600

    Updated API

--- src/main/java/edu/ulatina/APIEncryption/api/controller/EncryptionController.java
+++ src/main/java/edu/ulatina/APIEncryption/api/controller/EncryptionController.java
@@ -1,13 +1,15 @@
 package edu.ulatina.APIEncryption.api.controller;
 
 import edu.ulatina.APIEncryption.api.model.EncrypResponse;
-import edu.ulatina.APIEncryption.api.model.ErrorResponse;
 import edu.ulatina.APIEncryption.service.AESEncryptionDecryption;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -21,9 +23,7 @@ public class EncryptionController {
     }
 
     @GetMapping("/encryp")
-    public EncrypResponse getEncryp(@RequestParam String text) {
-        var response = new EncrypResponse();
-        var errorResponse = new ErrorResponse();
+    public ResponseEntity<EncrypResponse> getEncryp(@RequestParam String text) {
         try {
             var futureResponse = aes.encrypt(text);
 
@@ -31,40 +31,24 @@ public class EncryptionController {
                 TimeUnit.MILLISECONDS.sleep(100);
             }
 
-            response.setText(futureResponse.get());
-
-            errorResponse.setErrorMessage("Contraseña encriptada exitosamente");
-            errorResponse.setErrorCode(0);
+            return new ResponseEntity<>(new EncrypResponse(futureResponse.get()), HttpStatus.OK);
         } catch (Exception e) {
-            response.setText("");
-            errorResponse.setErrorMessage("Error al encriptar texto");
-            errorResponse.setErrorCode(-1);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        response.setErrorResponse(errorResponse);
-        return response;
     }
 
     @GetMapping("/dencryp")
-    public EncrypResponse getDencryp(@RequestParam String text) {
-        EncrypResponse response = new EncrypResponse();
-        ErrorResponse errorResponse = new ErrorResponse();
+    public ResponseEntity<EncrypResponse> getDencryp(@RequestParam String text) {
         try {
-            var futureResponse = aes.decrypt(text);
+            final Future<String> futureResponse = aes.decrypt(text);
 
             while (!futureResponse.isDone()) {
                 TimeUnit.MILLISECONDS.sleep(100);
             }
 
-            response.setText(futureResponse.get());
-
-            errorResponse.setErrorMessage("Texto desencriptado exitosamente");
-            errorResponse.setErrorCode(0);
+            return new ResponseEntity<>(new EncrypResponse(futureResponse.get()), HttpStatus.OK);
         } catch (Exception e) {
-            response.setText("");
-            errorResponse.setErrorMessage("Error al desencriptado texto");
-            errorResponse.setErrorCode(-1);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        response.setErrorResponse(errorResponse);
-        return response;
     }
 }
--- src/main/java/edu/ulatina/APIEncryption/api/model/EncrypResponse.java
+++ src/main/java/edu/ulatina/APIEncryption/api/model/EncrypResponse.java
@@ -2,14 +2,9 @@ package edu.ulatina.APIEncryption.api.model;
 
 public class EncrypResponse {
     private String text;
-    private ErrorResponse errorResponse;
 
-    public EncrypResponse() {
-    }
-
-    public EncrypResponse(String encryp, ErrorResponse errorResponse) {
-        this.text = encryp;
-        this.errorResponse = errorResponse;
+    public EncrypResponse(String text) {
+        this.text = text;
     }
 
     public String getText() {
@@ -18,13 +13,5 @@ public class EncrypResponse {
 
     public void setText(String text) {
         this.text = text;
-    }
-
-    public ErrorResponse getErrorResponse() {
-        return errorResponse;
-    }
-
-    public void setErrorResponse(ErrorResponse errorResponse) {
-        this.errorResponse = errorResponse;
     }
 }
